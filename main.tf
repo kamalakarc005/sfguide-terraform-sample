2de8c819698dcfd6681cdfb7e2e65ea5731b05fe
kamalakarc005 <kamal.reddy005@gmail.com>
2026-02-23 19:56:29 -0600

    Modified public key file path

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 locals {
   organization_name = "lhbrnah"
   account_name      = "ei45611"
-  private_key_path  = "~/.ssh/snowflake_tf_snow_key.p8"
+  private_key_path  = "C:\Users\kamal\Github\sfguide-terraform-sample\snowflake_tf_snow_key.p8"
 }
 
 provider "snowflake" {
